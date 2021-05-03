534878b0bcfd713512e691ea93016dad4743ec6b
Matthew Malensek <matt@malensek.net>
2021-05-03 15:51:29 -0700

    Fix bug w/ multiple Nonce lines

--- 04-Correct-Nonces-3.sh
+++ 04-Correct-Nonces-3.sh
@@ -6,7 +6,7 @@ mine_tester() {
     difficulty="${2}"
     output=$(./miner "${threads}" "${difficulty}" "${block}")
     echo "${output}"
-    nonce=$(grep 'Nonce:' <<< "${output}" | awk '{print $2}')
+    nonce=$(grep '^Nonce:' <<< "${output}" | awk '{print $2}')
     sha1=$(echo -n "${block}${nonce}" | sha1sum)
     zeros=0
     for i in $(sed -e 's/\(.\)/\1\n/g' <<< "${sha1}"); do
